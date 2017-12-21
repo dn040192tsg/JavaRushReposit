eeadfc2350a6a05676e63bb9a894659639ca376b
dn040192tsg <sturgelia@gmail.com>
2017-12-21 23:45:14 +0200

    new2

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task03.task0303;
+
+/* 
+Обмен валют
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println(convertEurToUsd(1, 33.40));
+        System.out.println(convertEurToUsd(1, 33.45));
+
+    }
+
+    public static double convertEurToUsd(int eur, double course) {
+        double dol = eur * course;
+        return dol;
+    }
+}
