3a15cc36b382f953a084d70ebd3b21d9ca88c5b3
dn040192tsg <sturgelia@gmail.com>
2017-12-22 00:55:04 +0200

    new3

--- 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
@@ -6,7 +6,70 @@ package com.javarush.task.task03.task0307;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Zerg zerg1 = new Zerg();
+        Zerg zerg2 = new Zerg();
+        Zerg zerg3 = new Zerg();
+        Zerg zerg4 = new Zerg();
+        Zerg zerg5 = new Zerg();
+        Zerg zerg6 = new Zerg();
+        Zerg zerg7 = new Zerg();
+        Zerg zerg8 = new Zerg();
+        Zerg zerg9 = new Zerg();
+        Zerg zerg10 = new Zerg();
+
+        zerg1.name = "1";
+        zerg2.name = "2";
+        zerg3.name = "3";
+        zerg4.name = "4";
+        zerg5.name = "5";
+        zerg6.name = "6";
+        zerg7.name = "7";
+        zerg8.name = "8";
+        zerg9.name = "9";
+        zerg10.name = "10";
+
+
+        Protoss toss1 = new Protoss();
+        Protoss toss2 = new Protoss();
+        Protoss toss3 = new Protoss();
+        Protoss toss4 = new Protoss();
+        Protoss toss5 = new Protoss();
+
+        toss1.name = "11";
+        toss2.name = "12";
+        toss3.name = "13";
+        toss4.name = "14";
+        toss5.name = "15";
+
+
+
+        Terran ter1 = new Terran();
+        Terran ter2 = new Terran();
+        Terran ter3 = new Terran();
+        Terran ter4 = new Terran();
+        Terran ter5 = new Terran();
+        Terran ter6 = new Terran();
+        Terran ter7 = new Terran();
+        Terran ter8 = new Terran();
+        Terran ter9 = new Terran();
+        Terran ter10 = new Terran();
+        Terran ter11 = new Terran();
+        Terran ter12 = new Terran();
+
+
+        ter1.name = "16";
+        ter2.name = "17";
+        ter3.name = "18";
+        ter4.name = "19";
+        ter5.name = "20";
+        ter6.name = "21";
+        ter7.name = "22";
+        ter8.name = "23";
+        ter9.name = "24";
+        ter10.name = "25";
+        ter11.name = "26";
+        ter12.name = "27";
+
     }
 
     public static class Zerg {
