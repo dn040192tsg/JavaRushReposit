97443e163af9061eed3a225befb1e2031135c01d
dn040192tsg <sturgelia@gmail.com>
2017-12-22 23:12:41 +0200

    new3

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task03.task0312;
+
+/* 
+Конвертируем время
+*/
+
+public class Solution {
+    //напишите тут ваш код
+
+    public static void main(String[] args) {
+
+        System.out.println(convertToSeconds(1));
+        System.out.println(convertToSeconds(2));
+
+    }
+
+    public static int convertToSeconds(int hour)
+    {
+        int sec = hour * 60;
+        return sec;
+    }
+}
