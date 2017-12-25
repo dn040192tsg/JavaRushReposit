3aedc1630623fcfeb471cc0724bc6edbe7bc8b4a
dn040192tsg <sturgelia@gmail.com>
2017-12-25 21:49:14 +0200

    new4

--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -9,9 +9,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = ;
+        int maximum;
 
         //напишите тут ваш код
+        int N = Integer.parseInt(reader.readLine());
+        if (N<=0) return;
+
+        int a;
+        maximum = Integer.parseInt(reader.readLine());
+        for (int i=0; i<N-1;i++) {
+            a = Integer.parseInt(reader.readLine());
+            if (a>maximum) {
+                maximum = a;
+            }
+        }
 
         System.out.println(maximum);
     }
