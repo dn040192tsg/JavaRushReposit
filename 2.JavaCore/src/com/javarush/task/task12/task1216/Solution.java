bd368b2b18be73edc277f039f2dfde959fb7d41c
dn040192tsg <sturgelia@gmail.com>
2018-01-04 23:53:41 +0200

    new4

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        int age (int a);
+
+        int age2 (int a);
+    }
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+interface Fly{
+   int rr (int a);
+}
+
+interface Run{
+    int rr (int a);
+}
+
+interface Swim{
+    int rr (int a);
+}
+
+}
