65067178db32030b9976d77e092ab53094aa0abf
dn040192tsg <sturgelia@gmail.com>
2018-01-11 00:01:45 +0200

    new4

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task13.task1326;
+
+/* 
+Сортировка четных чисел из файла
+*/
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream file = new FileInputStream("C:\\Users\\sturg\\Desktop\\rr.txt");
+
+
+        while (file.available() > 0){
+
+        }
+    }
+}
