3f9ad65ff7de6f370d08cf174097a4fcd917985c
dn040192tsg <sturgelia@gmail.com>
2018-01-16 22:36:20 +0200

    new4

--- 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
@@ -4,19 +4,41 @@ package com.javarush.task.task13.task1326;
 Сортировка четных чисел из файла
 */
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        // напишите тут ваш код
+        BufferedReader conReader = new BufferedReader(new InputStreamReader (System.in));
+        ArrayList<Integer> vect = new ArrayList<>();
 
-        FileInputStream file = new FileInputStream("C:\\Users\\sturg\\Desktop\\rr.txt");
+        try {
+            BufferedReader fileReader = new BufferedReader((new InputStreamReader(new FileInputStream(conReader.readLine()))));
 
-
-        while (file.available() > 0){
-
+            while (fileReader.ready()) {
+                vect.add(Integer.parseInt(fileReader.readLine()));
+            }
+            fileReader.close();
+        }catch (FileNotFoundException e){
+            System.out.println("File Not Found!");
+        } catch (IOException e) {
+            System.out.println("Can't read File");
         }
+
+        Collections.sort (vect);
+
+        for (Integer i : vect) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+
+
+
     }
 }
